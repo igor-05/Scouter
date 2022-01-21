929beb039b7f43601b01d70d1b22b466f40b90ef
Igor De Bock <igor.debock@gmail.com>
2022-01-21 23:28:57 +0000

    updated blink test with simpler code and video example

--- tests/blink/blink.ino
+++ tests/blink/blink.ino
@@ -1,19 +1,17 @@
+#define PIN 13
+
 /*
    Blink
    Turns on an LED on for one second, then off for one second, repeatedly.
 */
 
-// the setup function runs once when you press reset or power the board
-#define PIN (2) 
-
 void setup() { 
    pinMode(PIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
-
 void loop() {
-   digitalWrite(PIN, HIGH); // turn the LED on (HIGH is the voltage level)
-   delay(1000); // wait for a second
-   digitalWrite(PIN, LOW); // turn the LED off by making the voltage LOW
-   delay(1000); // wait for a second
+   digitalWrite(PIN, HIGH);
+   delay(1000); 
+   digitalWrite(PIN, LOW); 
+   delay(1000);
+}
